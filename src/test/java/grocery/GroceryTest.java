df00b3163053294213ca9b0eb48e4f72b3428860
Andric Quinn Baticos <andric.baticos@gcash.com>
2025-07-09 11:24:01 +0800

    add junit dependency, Unit Test initial template

---
+++ src/test/java/grocery/GroceryTest.java
@@ -0,0 +1,26 @@
+package grocery;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroceryTest {
+
+	@org.junit.jupiter.api.Test
+	void addProduct() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void viewInventory() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void checkProduct() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void updateProduct() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void removeProduct() {
+	}
+}
